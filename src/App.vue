16e019ee73d75381707ac70cd8e39f92243aa21f
teusbarros <smateusbp@gmail.com>
2020-11-17 22:35:45 -0300

    Mostra e carregar barra

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Barra :percentual="0"></Barra>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Barra from './components/Barra.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Barra
   }
 }
 </script>
---
+++ src/components/Barra.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="progress">
+      <div
+        class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+        role="progressbar"
+        :aria-valuenow="{ percentage }"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        :style="{ width: percentage + '%' }"
+      >{{percent}}%</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isConclued: this.conclued,
+      percentage: this.percentual,
+    };
+  },
+  props: {
+    id: String,
+    cor: String,
+    percentual: Number,
+    conclued: Boolean,
+  },
+  computed: {
+    percent() {
+      return this.percentage.toFixed();
+    }
+  },
+  created() {
+    var intval = setInterval(() => {
+      if(this.percentage < 100)
+        this.percentage += .01;
+      else
+        clearInterval(intval);
+    }, 10);
+  }
+};
+</script>
+
+<style scoped>
+.progress{
+    height: 50px !important;
+}
+.container {
+  text-align: right;
+  font-size: 8rem;
+  color: #555;
+}
+</style>
